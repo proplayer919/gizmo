db3f45de53b96738d629a221141f15e3b10986d2
Team 0x6861636B <95329795+proplayer919@users.noreply.github.com>
2023-11-16 17:57:21 +1100

    lots of formatting and cleanup

--- register-sw.js
+++ register-sw.js
@@ -14,17 +14,17 @@ const swAllowedHostnames = ["localhost", "127.0.0.1"];
  * Used in 404.html and index.html
  */
 async function registerSW() {
-  if (
-    location.protocol !== "https:" &&
-    !swAllowedHostnames.includes(location.hostname)
-  )
-    throw new Error("Service workers cannot be registered without https.");
+	if (
+		location.protocol !== "https:" &&
+		!swAllowedHostnames.includes(location.hostname)
+	)
+		throw new Error("Service workers cannot be registered without https.");
 
-  if (!navigator.serviceWorker)
-    throw new Error("Your browser doesn't support service workers.");
+	if (!navigator.serviceWorker)
+		throw new Error("Your browser doesn't support service workers.");
 
-  // Ultraviolet has a stock `sw.js` script.
-  await navigator.serviceWorker.register(stockSW, {
-    scope: __uv$config.prefix,
-  });
+	// Ultraviolet has a stock `sw.js` script.
+	await navigator.serviceWorker.register(stockSW, {
+		scope: __uv$config.prefix,
+	});
 }
--- scripts/index.js
+++ scripts/index.js
@@ -14,81 +14,81 @@ const searchEngine = document.getElementById("searchengine");
 
 
 form.addEventListener("submit", async (event) => {
-  event.preventDefault();
-  try {
-    await registerSW();
-  } catch (err) {
-    alert(err.toString());
-    throw err;
-  }
-  const url = search(address.value, searchEngine.value);
-      var white = document.createElement('div');
-            white.style.cursor="pointer";
-            white.style.position = "absolute";
-            white.style.width = "100%";
-            white.style.height = "100%";
-            white.style.zIndex="2147483647";
-            white.style.right = "0px";
-            white.className="black";
-            white.style.top = "0px";
-            document.body.appendChild(white);
+	event.preventDefault();
+	try {
+		await registerSW();
+	} catch (err) {
+		alert(err.toString());
+		throw err;
+	}
+	const url = search(address.value, searchEngine.value);
+	var white = document.createElement('div');
+	white.style.cursor = "pointer";
+	white.style.position = "absolute";
+	white.style.width = "100%";
+	white.style.height = "100%";
+	white.style.zIndex = "2147483647";
+	white.style.right = "0px";
+	white.className = "black";
+	white.style.top = "0px";
+	document.body.appendChild(white);
 
-            var loading = document.createElement('img');
-            loading.style.cursor="pointer";
-            loading.style.width = "125px";
-            loading.style.height = "125px";
-            loading.style.position="absolute";
-            loading.style.zIndex="2147483647"; 
-            loading.src = "/img/loading.gif";
-            loading.style.top="50%";
-            loading.style.left="50%";
-            loading.style.transform="translate(-50%, -50%)";
-            document.body.appendChild(loading);
+	var loading = document.createElement('img');
+	loading.style.cursor = "pointer";
+	loading.style.width = "125px";
+	loading.style.height = "125px";
+	loading.style.position = "absolute";
+	loading.style.zIndex = "2147483647";
+	loading.src = "/img/loading.gif";
+	loading.style.top = "50%";
+	loading.style.left = "50%";
+	loading.style.transform = "translate(-50%, -50%)";
+	document.body.appendChild(loading);
 
-            var iframe = document.createElement('iframe');
+	var iframe = document.createElement('iframe');
 
-            iframe.style.position = "absolute";
-            iframe.style.width = "100%";
-            iframe.style.height = "100%";
-            iframe.style.top = "0px";
-            iframe.style.left = "0px";
-            iframe.id = "iframe";
-            iframe.style.zIndex="9999999999999999";
-            iframe.style.border = "none";
-            iframe.src = __uv$config.prefix + __uv$config.encodeUrl(url);
-            document.body.appendChild(iframe);
+	iframe.style.position = "absolute";
+	iframe.style.width = "100%";
+	iframe.style.height = "100%";
+	iframe.style.top = "0px";
+	iframe.style.left = "0px";
+	iframe.id = "iframe";
+	iframe.style.zIndex = "9999999999999999";
+	iframe.style.border = "none";
+	iframe.src = __uv$config.prefix + __uv$config.encodeUrl(url);
+	document.body.appendChild(iframe);
 
-            var x = document.createElement('img');
-            x.style.cursor="pointer";
-            x.style.position = "absolute";
-            x.style.width = "50px";
-            x.style.height = "50px";
-            x.src = "/img/x.png";
-            x.style.zIndex = "99999999999999999999";
-            x.style.right = "1%";
-            x.style.top = "1%";
-            x.onclick = function() {
-                window.location.reload(1);
-            };
+	var x = document.createElement('img');
+	x.style.cursor = "pointer";
+	x.style.position = "absolute";
+	x.style.width = "50px";
+	x.style.height = "50px";
+	x.src = "/img/x.png";
+	x.style.zIndex = "99999999999999999999";
+	x.style.right = "1%";
+	x.style.top = "1%";
+	x.onclick = function () {
+		window.location.reload(1);
+	};
 
-            document.body.appendChild(x);
+	document.body.appendChild(x);
 
 
-            var open = document.createElement('img');
-            open.style.cursor="pointer";
-            open.style.position = "absolute";
-            open.style.width = "50px";
-            open.style.height = "50px";
-            open.src = "/img/open.png";
-            open.style.zIndex = "99999999999999999999";
-            open.style.right = "65px";
-            open.style.top = "1%";
-            open.onclick = function() {
-                if (document.getElementById("iframe").contentWindow.location.href!="about:blank"){
-                    window.open(document.getElementById("iframe").contentWindow.location.href);
-                }
-            };
+	var open = document.createElement('img');
+	open.style.cursor = "pointer";
+	open.style.position = "absolute";
+	open.style.width = "50px";
+	open.style.height = "50px";
+	open.src = "/img/open.png";
+	open.style.zIndex = "99999999999999999999";
+	open.style.right = "65px";
+	open.style.top = "1%";
+	open.onclick = function () {
+		if (document.getElementById("iframe").contentWindow.location.href != "about:blank") {
+			window.open(document.getElementById("iframe").contentWindow.location.href);
+		}
+	};
 
-            document.body.appendChild(open);
+	document.body.appendChild(open);
 
 });
