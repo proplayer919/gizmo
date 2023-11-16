6c4bd41777bb18bed54afe100c048994918f2160
Team 0x6861636B <95329795+proplayer919@users.noreply.github.com>
2023-11-16 17:49:59 +1100

    remove some analytics code and change some stuff

--- sw.js
+++ sw.js
@@ -3,18 +3,18 @@ var filesToCache = [
   '/sw.js'
 ];
 
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+    caches.open(cacheName).then(function (cache) {
       return cache.addAll(filesToCache);
     })
   );
   self.skipWaiting();
 });
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', function (e) {
   e.respondWith(
-    caches.match(e.request).then(function(response) {
+    caches.match(e.request).then(function (response) {
       return response || fetch(e.request);
     })
   );
